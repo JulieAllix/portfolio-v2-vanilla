262c6cf47274bb586f4c99f7fe17fb4e71fa79b8
JulieAllix <allixjulie@gmail.com>
2020-05-11 21:35:27 +0200

    Responsive design of photo.html done

---
+++ data/skillsData.js
@@ -0,0 +1,112 @@
+export default [
+    {
+        id: 1,
+        idName: 'react',
+        skill: 'React',
+        project: 'grocereaz',
+    },
+    {
+        id: 2,
+        idName: 'javaScript',
+        skill: 'JavaScript',
+        project: 'breakFree',
+    },
+    {
+        id: 3,
+        idName: 'htmlJsx',
+        skill: 'HTML, JSX',
+        project: 'studyCards',
+    },
+    {
+        id: 4,
+        idName: 'css',
+        skill: 'CSS3, Sass',
+        project: 'studyCards',
+    },
+    {
+        id: 5,
+        idName: 'axios',
+        skill: 'Axios',
+        project: 'grocereaz',
+    },
+    {
+        id: 6,
+        idName: 'redux',
+        skill: 'Redux',
+        project: 'grocereaz',
+    },
+    {
+        id: 7,
+        idName: 'sql',
+        skill: 'SQL',
+        project: 'breakFree',
+    },
+    {
+        id: 8,
+        idName: 'git',
+        skill: 'Git / Github',
+        project: 'grocereaz',
+    },
+    {
+        id: 9,
+        idName: 'agileMethod',
+        skill: 'Agile Method',
+        project: 'grocereaz',
+    },
+    {
+        id: 10,
+        idName: 'teamwork',
+        skill: 'Teamwork',
+        project: 'grocereaz',
+    },
+    {
+        id: 11,
+        idName: 'responsiveDesign',
+        skill: 'Responsive Design',
+        project: 'grocereaz',
+    },
+    {
+        id: 12,
+        idName: 'styledComponents',
+        skill: 'Styled Components',
+        project: 'grocereaz',
+    },
+    {
+        id: 13,
+        idName: 'semanticUI',
+        skill: 'Semantic UI',
+        project: 'grocereaz',
+    },
+    {
+        id: 14,
+        idName: 'apis',
+        skill: 'APIs',
+        project: 'grocereaz',
+    },
+    {
+        id: 15,
+        idName: 'cookies',
+        skill: 'Cookies',
+        project: 'breakFree',
+    },
+    {
+        id: 16,
+        idName: 'unitTests',
+        skill: 'Unit tests',
+        project: 'portfolio',
+    },
+    {
+        id: 17,
+        idName: 'db',
+        skill: 'Database',
+        project: 'breakFree',
+    },
+    {
+      id: 18,
+      idName: 'figma',
+      skill: 'Figma',
+      project: 'portfolioV3',
+    },
+    
+  ];
+  
