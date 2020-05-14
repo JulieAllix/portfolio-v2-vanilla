b757584fc40e7ac16776542aaafdb42a5bfb94d9
JulieAllix <allixjulie@gmail.com>
2020-05-14 13:07:45 +0200

    Improve animation for projects

--- app.js
+++ app.js
@@ -34,7 +34,10 @@ function init() {
         const nextRight = nextPage.querySelector('.hero .photo-right');
         const currentLeft = currentPage.querySelector('.hero .photo-left');
         const currentRight = currentPage.querySelector('.hero .photo-right');
+        const currentText = currentPage.querySelector('.detail');
         const nextText = nextPage.querySelector('.detail');
+        const currentProjects = currentPage.querySelector('.projects');
+        const nextProjects = nextPage.querySelector('.projects');
         const portfolio = document.querySelector('.portfolio');
 
         const tl = new TimelineMax({
@@ -53,17 +56,46 @@ function init() {
 
         // tl.fromTo(what I want to animate, the time, {y: '-10%'}, {y: '-100%'})
         // -=0.2 will make the animation start quicker : the second animation will not wait until the ed of the first animation
-        tl.fromTo(currentLeft, 0.3, {y: '-7%'}, {y: '-100%'})
+        if (currentProjects == null && nextProjects != null){
+        tl.fromTo(currentText, 0.2, {opacity: 1, y: 0}, {opacity: 0, y: -30})
+        .fromTo(currentLeft, 0.3, {y: '-7%'}, {y: '-100%'}, '-=0.2') // currentLeft dissappears
         .fromTo(currentRight, 0.3, {y: '7%'}, {y: '-100%'}, '-=0.2')
-        .to(portfolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
-        .fromTo(currentPage, 0.3, {opacity:1, pointerEvents: 'all'}, {opacity:0, pointerEvents: 'none'})
-        .fromTo(nextPage, 0.3, {opacity:0, pointerEvents: 'none'}, {opacity:1, pointerEvents: 'all'}, "-=0.6")
-        .fromTo(nextLeft, 0.3, {y: '-100%'}, {y: '-7%'}, '-=0.6')
+        .to(portfolio, 0.3, { backgroundImage: backgrounds[pageNumber] }) // update bg
+        .fromTo(currentPage, 0.3, {opacity:1, pointerEvents: 'all'}, {opacity:0, pointerEvents: 'none'}) // current page disappears
+        .fromTo(nextPage, 0.3, {opacity:0, pointerEvents: 'none'}, {opacity:1, pointerEvents: 'all'}, "-=0.6") // next page appears
+        .fromTo(nextLeft, 0.3, {y: '-100%'}, {y: '-7%'}, '-=0.6') // currentLeft appears
         .fromTo(nextRight, 0.3, {y: '-100%'}, {y: '7%'}, '-=0.8')
-        .fromTo(nextText, 0.3, {opacity: 0, y: 30}, {opacity: 1, y: 0})
+        .fromTo(nextProjects, 0.2, {opacity: 0, y: -30}, {opacity: 1, y: 0}, '-=0.2')
+        .fromTo(nextText, 0.3, {opacity: 0, y: 30}, {opacity: 1, y: 0}) //next text appears
         .set(nextLeft, {clearProps: 'all'})
         .set(nextRight, {clearProps: 'all'});
-
+        } else if (currentProjects != null && nextProjects == null){
+            tl.fromTo(currentText, 0.2, {opacity: 1, y: 0}, {opacity: 0, y: -30})
+            .fromTo(currentProjects, 0.2, {opacity: 1, y: 0}, {opacity: 0, y: 30}, '-=0.2')
+            .fromTo(currentLeft, 0.3, {y: '-7%'}, {y: '-100%'}, '-=0.2') // currentLeft dissappears
+            .fromTo(currentRight, 0.3, {y: '7%'}, {y: '-100%'}, '-=0.2')
+            .to(portfolio, 0.3, { backgroundImage: backgrounds[pageNumber] }) // update bg
+            .fromTo(currentPage, 0.3, {opacity:1, pointerEvents: 'all'}, {opacity:0, pointerEvents: 'none'}) // current page disappears
+            .fromTo(nextPage, 0.3, {opacity:0, pointerEvents: 'none'}, {opacity:1, pointerEvents: 'all'}, "-=0.6") // next page appears
+            .fromTo(nextLeft, 0.3, {y: '-100%'}, {y: '-7%'}, '-=0.6') // currentLeft appears
+            .fromTo(nextRight, 0.3, {y: '-100%'}, {y: '7%'}, '-=0.8')
+            .fromTo(nextText, 0.3, {opacity: 0, y: 30}, {opacity: 1, y: 0}) //next text appears
+            .set(nextLeft, {clearProps: 'all'})
+            .set(nextRight, {clearProps: 'all'});
+        }
+        else {
+            tl.fromTo(currentText, 0.2, {opacity: 1, y: 0}, {opacity: 0, y: -30})
+            .fromTo(currentLeft, 0.3, {y: '-7%'}, {y: '-100%'}, '-=0.2') // currentLeft dissappears
+            .fromTo(currentRight, 0.3, {y: '7%'}, {y: '-100%'}, '-=0.2')
+            .to(portfolio, 0.3, { backgroundImage: backgrounds[pageNumber] }) // update bg
+            .fromTo(currentPage, 0.3, {opacity:1, pointerEvents: 'all'}, {opacity:0, pointerEvents: 'none'}) // current page disappears
+            .fromTo(nextPage, 0.3, {opacity:0, pointerEvents: 'none'}, {opacity:1, pointerEvents: 'all'}, "-=0.6") // next page appears
+            .fromTo(nextLeft, 0.3, {y: '-100%'}, {y: '-7%'}, '-=0.6') // currentLeft appears
+            .fromTo(nextRight, 0.3, {y: '-100%'}, {y: '7%'}, '-=0.8')
+            .fromTo(nextText, 0.3, {opacity: 0, y: 30}, {opacity: 1, y: 0}) //next text appears
+            .set(nextLeft, {clearProps: 'all'})
+            .set(nextRight, {clearProps: 'all'});
+        }
         current = pageNumber;
         
     }
