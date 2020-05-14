52229a9ec9ba42b280b80489bcc278bc592e3062
JulieAllix <allixjulie@gmail.com>
2020-05-14 10:49:00 +0200

    Add tooltip and skills

--- data/skillsData.js
+++ data/skillsData.js
@@ -1,111 +1,138 @@
+import react from '../img/react.png';
+import js from '../img/javascript.png';
+import html from '../img/html5.png';
+import sass from '../img/sass.png';
+import axios from '../img/axios.png';
+import redux from '../img/redux.png';
+import sql from '../img/sql.png';
+import git from '../img/git.png';
+
 export default [
     {
         id: 1,
         idName: 'react',
         skill: 'React',
         project: 'grocereaz',
+        logo: react,
     },
     {
         id: 2,
         idName: 'javaScript',
         skill: 'JavaScript',
         project: 'breakFree',
+        logo: js,
     },
     {
         id: 3,
         idName: 'htmlJsx',
         skill: 'HTML, JSX',
         project: 'studyCards',
+        logo: html,
     },
     {
         id: 4,
         idName: 'css',
         skill: 'CSS3, Sass',
         project: 'studyCards',
+        logo: sass,
     },
     {
         id: 5,
         idName: 'axios',
         skill: 'Axios',
         project: 'grocereaz',
+        logo: axios,
     },
     {
         id: 6,
         idName: 'redux',
         skill: 'Redux',
         project: 'grocereaz',
+        logo: redux,
     },
     {
         id: 7,
         idName: 'sql',
         skill: 'SQL',
         project: 'breakFree',
+        logo: sql,
     },
     {
         id: 8,
         idName: 'git',
         skill: 'Git / Github',
         project: 'grocereaz',
+        logo: git,
     },
     {
         id: 9,
         idName: 'agileMethod',
         skill: 'Agile Method',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 10,
         idName: 'teamwork',
         skill: 'Teamwork',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 11,
         idName: 'responsiveDesign',
         skill: 'Responsive Design',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 12,
         idName: 'styledComponents',
         skill: 'Styled Components',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 13,
         idName: 'semanticUI',
         skill: 'Semantic UI',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 14,
         idName: 'apis',
         skill: 'APIs',
         project: 'grocereaz',
+        logo: '',
     },
     {
         id: 15,
         idName: 'cookies',
         skill: 'Cookies',
         project: 'breakFree',
+        logo: '',
     },
     {
         id: 16,
         idName: 'unitTests',
         skill: 'Unit tests',
         project: 'portfolio',
+        logo: '',
     },
     {
         id: 17,
         idName: 'db',
         skill: 'Database',
         project: 'breakFree',
+        logo: '',
     },
     {
       id: 18,
       idName: 'figma',
       skill: 'Figma',
       project: 'portfolioV3',
+      logo: '',
     },
     
   ];
